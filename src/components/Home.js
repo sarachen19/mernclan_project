fba212d516c3d248ba5d120e46ac3946f9668292
N01350427 <Sandeep.karedla1@gmail.com>
2021-04-22 16:38:13 -0400

    Notes App
    Added changes for Note component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,35 @@
+import '../components/Home.css';
+import React from 'react';
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
-    return (<h1>Home</h1> );
+
+    const history = useHistory();
+  
+    const handleRoute = () =>{ 
+      history.push("/login");
+    }
+
+     
+    const registerRoute = () =>{ 
+        history.push("/register");
+      }
+
+    return (
+        <div>
+        <div className = "title">
+        <h1>NoteWorks!!!! </h1>
+        </div>
+        <div className = "title">
+        <h1>The place where you note everything</h1>
+        </div>
+        <div className = "title chaddar">
+        <button type="button" class="btn btn-primary" onClick={handleRoute}>LogIn</button>
+        <button type="button" class="btn btn-primary" onClick={registerRoute}>Sign up Now</button>
+        </div>
+        </div>
+    
+    );
 }
  
 export default Home;
