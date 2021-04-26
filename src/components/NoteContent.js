ac5ef42396deea0309346c9d262093a1412b2fff
N01350427 <Sandeep.karedla1@gmail.com>
2021-04-26 01:35:04 -0400

    added files

--- src/components/NoteContent.js
+++ src/components/NoteContent.js
@@ -80,17 +80,14 @@ const NoteContent = ({ activeNote, onEditNote, test, textDisabled, setActiveNote
       if (cat === true){
         console.log("Parugu"+data.description);
         console.log("Parugu"+data.title);
-        if(data.description == ''){
-         
-          const r = test.find((note) => note._id === activeNote._id);
-         
-          data.description = "description";
+        
+        if(data.description == ""){
+          data.description = activeNote.description;
         }
-        if(data.title == ''){
-          const r = test.find((note) => note._id === activeNote._id);
-         
-          data.title = "Title";                 
+        if(data.title == ""){
+          data.title = activeNote.title;
         }
+       
           const response =  await axios.put(      
           'https://mern-clan.herokuapp.com/api/note',
           data,
